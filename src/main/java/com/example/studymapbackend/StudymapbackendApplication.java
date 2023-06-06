6202fc5ab78b4a368e05d083609c89633bfed0c0
Silver <kurriksilver@gmail.com>
2023-06-06 16:46:15 +0300

    First commit & push

---
+++ src/main/java/com/example/studymapbackend/StudymapbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studymapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymapbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymapbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studymapbackend/StudymapbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymapbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
